3dc30c6d484beec213d3cfd3128d8fab2d296074
hyunmin0317 <hyunmin-choi@naver.com>
2025-05-07 20:44:03 +0900

    feat: 다전공 관련 필드 추가 (#15)

--- src/main/java/com/smunity/dto/AuthResponseDto.java
+++ src/main/java/com/smunity/dto/AuthResponseDto.java
@@ -8,20 +8,29 @@ public record AuthResponseDto(
         String username,
         String name,
         String email,
-        String department
+        String department,
+        String secondDepartment,
+        boolean isDoubleMajor
 ) {
 
     public static AuthResponseDto from(JSONObject obj) {
+        String secondDepartment = getSecondDepartment(obj.optString("MJR_CD2"));
         return AuthResponseDto.builder()
                 .username(obj.getString("STDNO"))
                 .name(obj.getString("NM_KOR"))
                 .email(obj.getString("EMAIL"))
                 .department(getDepartment(obj.getString("TMP_DEPT_MJR_NM")))
+                .secondDepartment(secondDepartment)
+                .isDoubleMajor(secondDepartment != null)
                 .build();
     }
 
     private static String getDepartment(String dept) {
         String[] depts = dept.split(" ");
         return depts[depts.length - 1];
+    }
+
+    private static String getSecondDepartment(String dept) {
+        return dept.isBlank() ? null : getDepartment(dept);
     }
 }
