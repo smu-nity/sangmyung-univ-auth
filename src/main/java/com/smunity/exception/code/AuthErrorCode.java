293f244ea443eac86ca803a154dac17b08cbbd43
hyunmin0317 <hyunmin-choi@naver.com>
2026-01-28 19:40:23 +0900

    chore: 에러 코드 수정 (#23)

--- src/main/java/com/smunity/exception/code/AuthErrorCode.java
+++ src/main/java/com/smunity/exception/code/AuthErrorCode.java
@@ -9,9 +9,9 @@ public enum AuthErrorCode {
 
     // Auth Errors
     AUTH_UNAUTHORIZED(401, "AUTH001", "학번 및 비밀번호가 일치하지 않습니다."),
-    AUTH_LOGIN_FAIL(401, "AUTH002", "샘물포털 로그인에 실패했습니다."),
-    AUTH_FETCH_FAILURE(401, "AUTH003", "인증 서버 에러, 관리자에게 문의 바랍니다."),
-    AUTH_EXCEEDED_LOGIN_ATTEMPTS(401, "AUTH004", "로그인 실패 5회 초과, 샘물 포탈을 통해 비밀번호 초기화를 진행해주시기 바랍니다.");
+    AUTH_EXCEEDED_LOGIN_ATTEMPTS(423, "AUTH002", "로그인 실패 5회 초과, 샘물 포탈을 통해 비밀번호 초기화를 진행해주시기 바랍니다."),
+    SMU_LOGIN_FAIL(503, "SMU001", "샘물포털 인증 서버에 일시적으로 연결할 수 없습니다. 잠시 후 다시 시도해 주세요."),
+    SMU_FETCH_FAILURE(503, "SMU002", "샘물포털 서버에서 데이터를 불러올 수 없습니다. 잠시 후 다시 시도해 주세요.");
 
     private final int value;
     private final String code;
