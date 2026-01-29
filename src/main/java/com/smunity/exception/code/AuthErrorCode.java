affa13fc32c09fde8d82046610eb6a518c057098
Hyunmin Choi <hyunmin-choi@naver.com>
2026-01-29 18:51:36 +0900

    [BUG] 장기간 비밀번호 미변경 사용자 인증 실패 시 서버 오류로 처리되는 문제 (#30)
    * feat: 에러 코드 추가 (#29)
    
    * fix: 장기간 비밀번호 미변경 사용자 인증 실패 시 서버 오류로 처리되는 문제 (#29)

--- src/main/java/com/smunity/exception/code/AuthErrorCode.java
+++ src/main/java/com/smunity/exception/code/AuthErrorCode.java
@@ -7,9 +7,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AuthErrorCode implements BaseCode {
 
-    // Auth Errors
+    // Client Errors
     AUTH_UNAUTHORIZED(401, "AUTH001", "학번 및 비밀번호가 일치하지 않습니다."),
     AUTH_EXCEEDED_LOGIN_ATTEMPTS(423, "AUTH002", "로그인 실패 5회 초과, 샘물 포탈을 통해 비밀번호 초기화를 진행해주시기 바랍니다."),
+    AUTH_PASSWORD_EXPIRED(423, "AUTH003", "장기간 비밀번호를 변경하지 않으셨습니다. 샘물 포탈을 통해 비밀번호 변경 후 로그인해주시기 바랍니다."),
+
+    // Server Errors
     SMU_LOGIN_FAILURE(503, "SMU001", "샘물포털 인증 서버에 일시적으로 연결할 수 없습니다. 잠시 후 다시 시도해 주세요."),
     SMU_FETCH_FAILURE(503, "SMU002", "샘물포털 서버에서 데이터를 불러올 수 없습니다. 잠시 후 다시 시도해 주세요."),
     SMU_LOGIN_TIMEOUT(504, "SMU003", "샘물포털 인증 서버 응답 시간이 초과되었습니다. 잠시 후 다시 시도해 주세요."),
